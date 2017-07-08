9100d3aa377ca0cc9fa65e955d748334bcb721b7
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-07-09 00:55:15 +0200

    Email

--- start.sh
+++ start.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-# emailcheck v0.1
+# emailcheck v0.2
 # Made by Dr. Waldijk
 # Check if an email exists or not.
 # Read the README.md for more info.
 # By running this script you agree to the license terms.
-# -----------------------------------------------------------------------------------
+# Standard --------------------------------------------------------------------------
 EMCHNAM="emailcheck"
-EMCHVER="0.1"
+EMCHVER="0.2"
+# Config ----------------------------------------------------------------------------
+EMCHYOU=$1
+EMCHCHK=$2
+# -----------------------------------------------------------------------------------
 # telnet smtp.domain.com 25 << EOF
 # echo "HELO domain.com"
 # echo "MAIL FROM: <your@domain.com>"
 # echo "RCPT TO: <check@domain.com>"
 # echo "quit"
 # EOF
-{
-#    sleep 3
-    echo "HELO domain.com"
-#    sleep 3
-    echo "MAIL FROM: <your@domain.com>"
-#    sleep 3
-    echo "RCPT TO: <check@domain.com>"
-#    sleep 3
-    echo "quit"
-} | telnet smtp.domain.com 25 | tee -a -i test.log
+if [ -n "$EMCHYOU" ] && [ -n "$EMCHCHK" ]; then
+    EMCHHEL=$(echo $EMCHYOU | sed -r 's/.*@(.*)/\1/')
+    EMCHDOM=$(echo $EMCHCHK | sed -r 's/.*@(.*)/\1/')
+#    EMCHSMTP=$(nslookup -type=MX $EMCHDOM | grep ' 5 ' | sed -r 's/.*\s5\s(.*)\./\1/')
+    EMCHSMTP=$(nslookup -type=MX $EMCHDOM | grep 'mail exchanger' | sed -r 's/.*=\s(.*)/\1/g' | sort -n | sed -r 's/.*\s(.*)/\1/g' | sed -n '1p')
+    EMCHFIL=$(echo "$EMCHCHK" | sed -r 's/@//' | sed -r 's/\.//g' | sed -r 's/-//g' | sed -r 's/_//g')
+#    echo "[$EMCHCHK]" | tee -a -i $EMCHFIL.log
+    {
+        sleep 3
+        echo "HELO $EMCHHEL"
+        sleep 1
+        echo "MAIL FROM: <$EMCHYOU>"
+        sleep 1
+        echo "RCPT TO: <$EMCHCHK>"
+        sleep 1
+        echo "QUIT"
+    } | tee -a -i $EMCHFIL.log | telnet $EMCHSMTP 25 | tee -a -i $EMCHFIL.log
+else
+    echo "$EMCHNAM v$EMCHVER"
+    echo "emailcheck <your email address> <email address you want to verify>"
+fi
